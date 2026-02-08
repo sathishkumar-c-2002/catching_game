b7554db6301ca33d36f016f9c257b168a9e80f4f
csathishkumar <csathishkumar2002@gmail.com>
2026-02-08 20:58:39 +0530

    Glove Animation Created and Random ball Position Created

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -11,9 +11,13 @@ class GameScreen extends Phaser.Scene {
     create() {
         console.log("Game Screen")
 
+        let touch_side;
+        let ball_x;
+        let ball_y;
+        let touch_state = false;
+
         let background = this.add.image(180, 265, 'gamescreen_background');
         background.setScale(0.51);
-
 
         // // Left Top
         let score_board_bg = this.add.image(50, 50, 'score_board_bg');
@@ -51,32 +55,43 @@ class GameScreen extends Phaser.Scene {
                 y: y,
                 scale: 0.6,
                 angle: 360,
-                duration: 1000,
+                duration: 1500,
                 ease: 'Cubic.out'
             }
             return return_map;
-
         }
-        // Player Character - Left angle
-        let char_left = this.add.image(400, 330, 'char_left');
-        char_left.setScale(0.8);
-        this.tweens.add({
-            targets: char_left,
-            x: 200,
-            duration: 1000,
-            ease: 'Power2',
-            onComplete: () => {
-                // Ball kick animation
-                this.tweens.add(BallRotation(left_ball, 70, 500));
-                score += 1;
-                score_text.setText(score.toString().padStart(2, '0'));
-            }
-        });
 
-        let left_ball = this.add.image(160, 400, 'ball');
-        left_ball.setScale(0.3);
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-        // Player Character - Right angle
+        // // Player Character - Left angle
+        // let char_left = this.add.image(400, 330, 'char_left');
+        // char_left.setScale(0.8);
+        // this.tweens.add({
+        //     targets: char_left,
+        //     x: 200,
+        //     duration: 1000,
+        //     ease: 'Power2',
+        //     onComplete: () => {
+        //         // Ball kick animation
+        //         // this.tweens.add(BallRotation(left_ball, 70, 500));
+        //         let random_x = getRandomInt(30, 150);
+        //         let random_y = getRandomInt(400, 600);
+
+        //         this.tweens.add(BallRotation(left_ball, random_x, random_y));
+
+        //         score += 1;
+        //         score_text.setText(score.toString().padStart(2, '0'));
+        //     }
+        // });
+
+        // let left_ball = this.add.image(160, 400, 'ball');
+        // left_ball.setScale(0.3);
+
+        // // Player Character - Right angle
         let char_right = this.add.image(-100, 330, 'char_right');
         char_right.setScale(0.8);
         let right_ball = this.add.image(210, 400, 'ball');
@@ -88,15 +103,12 @@ class GameScreen extends Phaser.Scene {
             ease: 'Power2',
             onComplete: () => {
                 // Ball kick animation
-                this.tweens.add({
-                    targets: right_ball,
-                    x: 310,
-                    y: 500,
-                    scale: 0.6,
-                    angle: 360,
-                    duration: 1000,
-                    ease: 'Cubic.out'
-                });
+                // this.tweens.add(BallRotation(left_ball, 70, 500));
+                let random_x = getRandomInt(210, 350);
+                let random_y = getRandomInt(400, 600);
+
+                this.tweens.add(BallRotation(right_ball, random_x, random_y));
+
                 score += 1;
                 score_text.setText(score.toString().padStart(2, '0'));
             }
@@ -104,18 +116,34 @@ class GameScreen extends Phaser.Scene {
 
 
 
+
+        this.input.on('pointerdown', (pointer) => {
+            touch_side = "left"
+            if (pointer.x > 180) {
+                touch_side = "right"
+            }
+            ball_x = pointer.x;
+            ball_y = pointer.y;
+            // alert(`X: ${pointer.x}, Y: ${pointer.y}`);
+            // alert(`side = ${touch_side} points x: ${pointer.x} y: ${pointer.y}`)
+        });
+
         let gloves = this.add.image(180, 530, 'gloves');
         gloves.setScale(0.3);
+        this.input.on('pointerdown', () => {
 
+            if (ball_x != null && ball_y != null && touch_side != null) {
+                this.tweens.add({
+                    targets: gloves,
+                    x: ball_x,
+                    y: ball_y,
+                    angle: touch_side === 'left' ? -20 : 20,
+                    duration: 200,
+                    ease: 'Power2'
+                });
+            }
 
-
-
-
-
-
-
-
-
+        });
 
 
     }
